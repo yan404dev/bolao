a93ef7317d31e6488e520b5b2d49e094ee30c113
yan404dev <yancastrohenrique1@gmail.com>
2026-01-20 08:50:20 -0300

    ajust import

--- apps/api/src/main/java/com/bolao/shared/config/GlobalExceptionHandler.java
+++ apps/api/src/main/java/com/bolao/shared/config/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
